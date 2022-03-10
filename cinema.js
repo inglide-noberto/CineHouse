b420b2538b0c17ab423d1b6dc710ca357a7763a3
Inglide <inglidenoberto961@gmail.com>
2022-03-10 13:17:11 -0300

    iniciando desafio

---
+++ cinema.js
@@ -0,0 +1,38 @@
+const cinema = 'CineHouse';
+//console.log(cinema);
+
+var catalogo = [
+    { 
+        codigo: 1 ,
+        titulo: 'Crepusculo' ,
+        duracao: 120 ,
+        atores: ['Robert Pattison', 'Taylor Lautner', 'Kristen Stewart'] , 
+        anoDeLancamento: 2008 , 
+        emCartaz: false ,
+    } ,
+    { 
+        codigo: 2 ,
+        titulo: 'Lua Nova' ,
+        duracao: 90 ,
+        atores: ['Robert Pattison', 'Taylor Lautner', 'Kristen Stewart'] , 
+        anoDeLancamento: 2010 ,  
+        emCartaz: false ,
+    }
+];
+//console.log(catalogo);
+function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz){
+    catalogo.push({codigo, titulo, duracao, atores, anoDeLancamento, emCartaz});
+
+}
+adicionarFilme(3, "Harry Potter",150, ["Daniel Radcliff", "Emma Watson", "Rupert Grint"], 2001, false);
+
+//console.log(catalogo);
+function buscarFilme(codigo){
+   if(catalogo[codigo].includes(catalogo)){
+        indice = catalogo.indexOf(codigo);
+
+        return catalogo[{codigo}];
+   }
+   
+}
+console.log(buscarFilme(2));
