133c83fed28b51297ace31661650fc48af12e0fb
Inglide <inglidenoberto961@gmail.com>
2022-03-10 21:21:54 -0300

    trabalhando com json

--- cinema.js
+++ cinema.js
@@ -1,38 +1,28 @@
-const cinema = 'CineHouse';
-//console.log(cinema);
+const minhaString = '      Aula';
 
-var catalogo = [
-    { 
-        codigo: 1 ,
-        titulo: 'Crepusculo' ,
-        duracao: 120 ,
-        atores: ['Robert Pattison', 'Taylor Lautner', 'Kristen Stewart'] , 
-        anoDeLancamento: 2008 , 
-        emCartaz: false ,
-    } ,
-    { 
-        codigo: 2 ,
-        titulo: 'Lua Nova' ,
-        duracao: 90 ,
-        atores: ['Robert Pattison', 'Taylor Lautner', 'Kristen Stewart'] , 
-        anoDeLancamento: 2010 ,  
-        emCartaz: false ,
-    }
-];
-//console.log(catalogo);
-function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz){
-    catalogo.push({codigo, titulo, duracao, atores, anoDeLancamento, emCartaz});
+console.log(minhaString.trim());
 
-}
-adicionarFilme(3, "Harry Potter",150, ["Daniel Radcliff", "Emma Watson", "Rupert Grint"], 2001, false);
+//const catalogo = require("./database/catalogo.json");
 
-//console.log(catalogo);
-function buscarFilme(codigo){
-   for (let i= 0; i<= catalogo.length; i++){
-       if(codigo == catalogo[i].codigo){
-           return catalogo[i];
-       }
-   }
-   
-}
-console.log(buscarFilme(2));
+// function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz){
+//     catalogo.push({codigo, titulo, duracao, atores, anoDeLancamento, emCartaz});
+
+// }
+// adicionarFilme(3, "Harry Potter",150, ["Daniel Radcliff", "Emma Watson", "Rupert Grint"], 2001, false);
+
+// //console.log(catalogo);
+// function buscarFilme(codigo){
+//     for (let i= 0; i<= catalogo.length; i++){
+//         if(codigo == catalogo[i].codigo){
+//            return catalogo[i];
+//         }
+//     }  
+// }
+// function alterarStatusEmCartaz(numero){
+//     for (let i= 0; i<= catalogo.length; i++){
+//         if(catalogo[i].codigo == numero){
+//             catalogo[i].emCartaz = !catalogo[i].emCartaz;
+//         }
+//     }
+//  }
+// console.log(catalogo[0])
