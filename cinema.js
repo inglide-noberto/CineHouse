6cddab92e4326f001ff06a5108cfb78aebf07bbb
Inglide <inglidenoberto961@gmail.com>
2022-03-10 22:00:19 -0300

    aula finalizada

--- cinema.js
+++ cinema.js
@@ -1,28 +1,37 @@
-const minhaString = '      Aula';
+const minhaString = 'Aula';
+const alunos = [
+    "      Marcos",
+    "      Julio",
+    "      Eder"
+]
+//for (let i= 0; i<= alunos.length; i++){
+  //        console.log(alunos[i].trim());
+    //    }  
 
-console.log(minhaString.trim());
+//console.log(minhaString.trim());
 
-//const catalogo = require("./database/catalogo.json");
+const catalogo = require("./database/catalogo.json");
 
-// function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz){
-//     catalogo.push({codigo, titulo, duracao, atores, anoDeLancamento, emCartaz});
+function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz){
+    catalogo.push({codigo, titulo, duracao, atores, anoDeLancamento, emCartaz});
 
-// }
-// adicionarFilme(3, "Harry Potter",150, ["Daniel Radcliff", "Emma Watson", "Rupert Grint"], 2001, false);
+}
+adicionarFilme(3, "Harry Potter",150, ["Daniel Radcliff", "Emma Watson", "Rupert Grint"], 2001, false);
 
-// //console.log(catalogo);
-// function buscarFilme(codigo){
-//     for (let i= 0; i<= catalogo.length; i++){
-//         if(codigo == catalogo[i].codigo){
-//            return catalogo[i];
-//         }
-//     }  
-// }
-// function alterarStatusEmCartaz(numero){
-//     for (let i= 0; i<= catalogo.length; i++){
-//         if(catalogo[i].codigo == numero){
-//             catalogo[i].emCartaz = !catalogo[i].emCartaz;
-//         }
-//     }
-//  }
-// console.log(catalogo[0])
+//console.log(catalogo);
+function buscarFilme(codigo){
+    for (let i= 0; i<= catalogo.length; i++){
+        if(codigo == catalogo[i].codigo){
+           return catalogo[i];
+        }
+    }  
+}
+function alterarStatusEmCartaz(codigo){
+    for (let i= 0; i<= catalogo.length; i++){
+        if(catalogo[i].codigo == codigo){
+            catalogo[i].emCartaz = !catalogo[i].emCartaz;
+        }
+    }
+ }
+ alterarStatusEmCartaz(2);
+ console.log(catalogo)
